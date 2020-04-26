d28a14a4aabb05b35e842a7202afc58a161ae7a0
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-26 00:39:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-if value<=1
-puts name_hash
 
+def key_for_min_value(name_hash)
+  lowest_value = 0
+  lowest_key = nil
+   name_hash.each do |key,value|
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
